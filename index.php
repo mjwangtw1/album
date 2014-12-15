ba09646ee5aa4e434ebe26e903cffc4009f1a484
Mou.Wang <mjwangtw@gmail.com>
2014-12-15 17:33:01 +0800

    Standardized HTML format. change font to Georgia

--- index.php
+++ index.php
@@ -10,30 +10,60 @@
 
 ?>
 
-<link rel="stylesheet" type="text/css" href="./css/lib/sweet-alert.css">
+
+<!DOCTYPE HTML PUBLIC >
+<HTML>
+   <HEAD>
+      <TITLE>Mou's Album (in development)</TITLE>
+   </HEAD>
+   <BODY>
 
 
-<div class="thumb_zone">
-<span class="thumb">
-<img src="https://farm4.staticflickr.com/3755/10565638896_530c0595c5_s.jpg" width="75" height="75" alt="Bar@Pittsburgh" value="img1">
 
-<img src="https://farm8.staticflickr.com/7323/10565637196_2296f583fd_s.jpg" width="75" height="75" alt="Famous Primantis sandwich" value="img2">
-
-<img src="https://farm3.staticflickr.com/2851/10565986203_8255a93880_s.jpg" width="75" height="75" alt="Robber duck and ducklings" value="img3">
- 
-</span>
-<span class="desc"></span>
-</div>
-<div class="show_midsize_photo">  
-    <img class="show" src="">
-    
-</div>
-
-<!-- <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script> -->
-
-<!-- LIB-->
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="./js/lib/sweet-alert.min.js"></script>
+	<!-- CSS LIB -->
+	<link rel="stylesheet" type="text/css" href="./css/lib/sweet-alert.css">
+	<!-- CSS Customize -->
+	<link rel="stylesheet" type="text/css" href="./css/album.css">
 
 
-<script type="text/javascript" src="./js/album.js"></script>
+	<div class="thumb_zone">
+	<span class="thumb">
+		<img src="https://farm4.staticflickr.com/3755/10565638896_530c0595c5_s.jpg" width="75" height="75" alt="Bar@Pittsburgh" value="img1">
+
+		<img src="https://farm8.staticflickr.com/7323/10565637196_2296f583fd_s.jpg" width="75" height="75" alt="Famous Primantis sandwich" value="img2">
+
+		<img src="https://farm3.staticflickr.com/2851/10565986203_8255a93880_s.jpg" width="75" height="75" alt="Robber duck and ducklings" value="img3">
+	 
+	</span>
+	<span class="desc"></span>
+	</div>
+	<div class="show_midsize_photo">  
+	    <img class="show" src="">
+	    
+	</div>
+
+	<!-- <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script> -->
+
+	<!--  JS - LIB-->
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="./js/lib/sweet-alert.min.js"></script>
+
+	<!-- JS - custom-->
+	<script type="text/javascript" src="./js/album.js"></script>
+
+
+
+
+
+
+
+
+
+
+   </BODY>
+</HTML>
+
+
+
+
+
